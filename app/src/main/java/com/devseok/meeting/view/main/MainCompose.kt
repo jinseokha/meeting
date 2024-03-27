e5bd8a2d20320627fe828feb72d3a86bb34c3b13
jinseokha <seok270@gmail.com>
2024-03-27 16:47:30 +0900

    [EDIT] : MainCompose -> width, height

--- app/src/main/java/com/devseok/meeting/view/main/MainCompose.kt
+++ app/src/main/java/com/devseok/meeting/view/main/MainCompose.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -38,6 +39,8 @@ fun MainScreen() {
     Box(
         modifier = Modifier
             .padding(15.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
     ) {
         FloatingActionButton(
             onClick = {
